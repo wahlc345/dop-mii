1d36fc368f2b6fafc18f28e77a0a6ccf283035e2
castlevania7689@yahoo.com <castlevania7689@yahoo.com@442a8530-2689-11df-8747-af52ddc49692>
2009-11-02 21:28:50 +0000

    Dop-IOS MOD v6

--- trunk/source/dopios.c
+++ trunk/source/dopios.c
@@ -65,33 +65,34 @@ struct ios{
 };
 
 
-const struct ios ioses[]={
+   const struct ios ioses[]={
+// IOS# OLDVERSION# NEWVERSION#, ???, Description -- Arikado
 	{4,65280,65280,PROTECTED,"Stub, useless now."},
-	{9,520,521,NORMAL,"Used by launch titles (Zelda: Twilight Princess) and System Menu 1.0."},
+	{9,520,778,NORMAL,"Used by launch titles (Zelda: Twilight Princess) and System Menu 1.0."},
 	{10,768,768,PROTECTED,"Stub, useless now."},
 	{11,10,256,STUB_NOW,"Used only by System Menu 2.0 and 2.1."},
-	{12,6,12,NORMAL,""},
-	{13,10,16,NORMAL,""},
-	{14,262,263,NORMAL,""},
-	{15,257,266,NORMAL,""},
+	{12,6,269,NORMAL,""},
+	{13,10,273,NORMAL,""},
+	{14,262,520,NORMAL,""},
+	{15,257,523,NORMAL,""},
 	{16,512,512,PROTECTED,"Piracy prevention stub, useless."},
-	{17,512,518,NORMAL,""},
+	{17,512,775,NORMAL,""},
 	{20,12,0,STUB_NOW,"Used only by System Menu 2.2."},
-	{21,514,525,NORMAL,"Used by old third-party titles (No More Heroes)."},
-	{22,777,780,NORMAL,""},
-	{28,1292,1293,NORMAL,""},
+	{21,514,782,NORMAL,"Used by old third-party titles (No More Heroes)."},
+	{22,777,1037,NORMAL,""},
+	{28,1292,1550,NORMAL,""},
 	{30,1040,2816,STUB_NOW,"Used only by System Menu 3.0, 3.1, 3.2 and 3.3."},
-	{31,1040,3092,NORMAL,""},
-	{33,1040,2834,NORMAL,""},
-	{34,1039,3091,NORMAL,""},
-	{35,1040,3092,NORMAL,"Used by: Super Mario Galaxy."},
-	{36,1042,3094,NORMAL,"Used by: Smash Bros. Brawl, Mario Kart Wii. Can be ES_Identify patched."},
-	{37,2070,3612,NORMAL,"Used mostly by music games (Guitar Hero)."},
+	{31,1040,3349,NORMAL,""},
+	{33,1040,3091,NORMAL,""},
+	{34,1039,3348,NORMAL,""},
+	{35,1040,3349,NORMAL,"Used by: Super Mario Galaxy."},
+	{36,1042,3351,NORMAL,"Used by: Smash Bros. Brawl, Mario Kart Wii. Can be ES_Identify patched."},
+	{37,2070,3869,NORMAL,"Used mostly by music games (Guitar Hero)."},
 	{38,3610,3867,NORMAL,"Used by some modern titles (Animal Crossing)."},
 	{50,4889,5120,STUB_NOW,"Used only by System Menu 3.4."},
 	{51,4633,4864,STUB_NOW,"Used only by Shop Channel 3.4."},
-	{53,4113,5149,NORMAL,"Used by some modern games and channels."},
-	{55,4633,5149,NORMAL,"Used by some modern games and channels."},
+	{53,4113,5406,NORMAL,"Used by some modern games and channels."},
+	{55,4633,5406,NORMAL,"Used by some modern games and channels."},
 	{56,4890,5405,NORMAL,"Used only by Wii Speak Channel 2.0."},
 	{57,5404,5661,NORMAL,"Unknown yet."},
 	{60,6174,6400,STUB_NOW,"Used by System Menu 4.0 and 4.1."},
@@ -102,9 +103,7 @@ const struct ios ioses[]={
 	{249,65280,65280,PROTECTED,"Piracy prevention stub, useless."},
 	{250,65280,65280,PROTECTED,"Piracy prevention stub, useless."},
 	{254,2,260,PROTECTED,"PatchMii prevention stub, useless."}
-};
-	
-	
+   };
 
 u32 ios_found[256];
 
@@ -127,7 +126,7 @@ void printMyTitle(){
 	setConsoleBgColor(BLUE,0);
 	setConsoleFgColor(WHITE,1);
 	printf("                                                                        ");
-	printf("                             Dop-IOS MOD v5                             ");
+	printf("                             Dop-IOS MOD v6                             ");
 	printf("                                                                        ");
 	setConsoleBgColor(BLACK,0);
 	setConsoleFgColor(WHITE,0);
