981dd91274e07b8aa3479af194e448de90a5daeb
Lunatik.CN <Lunatik.CN@442a8530-2689-11df-8747-af52ddc49692>
2010-01-04 00:32:01 +0000

    Commented out PNG Rendering as we are not using it right now.

--- source/video.h
+++ source/video.h
@@ -1,7 +1,7 @@
 #ifndef _VIDEO_H_
 #define _VIDEO_H_
 
-#include <pngu/pngu.h>
+//#include <pngu/pngu.h>
 
 /* Prototypes */
 void Con_Init(u32, u32, u32, u32);
@@ -14,7 +14,7 @@ void Con_FillRow(u32, u32, u8);
 void Video_Configure(GXRModeObj *);
 void Video_SetMode(void);
 void Video_Clear(s32);
-void Video_DrawPng(IMGCTX, PNGUPROP, u16, u16);
+//void Video_DrawPng(IMGCTX, PNGUPROP, u16, u16);
 
 #endif
 
