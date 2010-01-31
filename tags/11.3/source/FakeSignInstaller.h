47ae36d2ba934b8a1b606a88fa7785d0ccf13630
Lunatik.CN <Lunatik.CN@442a8530-2689-11df-8747-af52ddc49692>
2010-01-31 03:32:06 +0000

    Tagged 11.3

---
+++ tags/11.3/source/FakeSignInstaller.h
@@ -0,0 +1,17 @@
+#ifndef _FAKESIGNINSTALLER_H_
+#define _FAKESIGNINSTALLER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define IOS15Version 523
+#define IOS36Version 3351
+
+int FakeSignInstall();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
