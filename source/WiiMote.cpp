3f4bce98facf2feb807f1dd5e46395384259134a
castlevania7689 <castlevania7689@yahoo.com>
2010-11-23 01:28:22 +0000

    Apparently some of our LED light magic doesnt cooperate with libogc 1.8.5 either causing huge crashes so Im disabling it (for now).

--- source/WiiMote.cpp
+++ source/WiiMote.cpp
@@ -17,7 +17,7 @@
 
 
 void WiiMote::SetLeds(int chan, int wiimote_led_x)
-{
+{/*
 	int level;
 	_CPU_ISR_Disable(level);
 	struct wiimote_t **__wpads = *((struct wiimote_t***)__WPADS_ADDR);
@@ -27,4 +27,5 @@ void WiiMote::SetLeds(int chan, int wiimote_led_x)
 	if (status == WPAD_ERR_NONE && wm->leds != 0) wiiuse_set_leds(wm, wiimote_led_x, NULL);
 	else if (status == WPAD_ERR_NO_CONTROLLER) wm->leds = 0;
 	_CPU_ISR_Restore(level);	
+	*/
 }
